e2fc733dbfcac3596ce05ce2f05f3d8ae33793a8
ProtossDragoon <dlwkdgn1@naver.com>
2023-01-15 21:42:44 +0100

    test: 그리디가 실패하는 경우 테스트케이스 및 주석추가

--- change_test.py
+++ change_test.py
@@ -38,7 +38,17 @@ class TestChange(unittest.TestCase):
         self.assertEqual(c100, 2)
         self.assertEqual(c50, 1)
         self.assertEqual(c10, 4)
-
+    def test_not_ideal(self):
+        alg = change.Algorithm(120, coin_types={10, 60, 100})
+        c100, c60, c10 = alg.calculate()
+        with self.subTest('우리의 알고리즘'):
+            self.assertEqual(c100, 1)
+            self.assertEqual(c60, 0)
+            self.assertEqual(c10, 2)
+        """with self.subTest('이상적인 경우'):
+            self.assertEqual(c100, 0)
+            self.assertEqual(c60, 2)
+            self.assertEqual(c10, 0)"""
 
 if __name__ == "__main__":
     unittest.main()
